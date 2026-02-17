220ebdeff31a26bfea56d81bd081da21bb3f176a
aragulkumar <aragulkumar11@gmail.com>
2026-02-17 12:46:00 +0530

    feat: use blue logo in navbar/about and original logo in hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
                         </p>
                     </div>
                     <div className="about-logo">
-                        <img src="/heisenbyte-logo.png" alt="Heisenbyte Logo" className="about-logo-image" />
+                        <img src="/heisenbyte-logo-blue.png" alt="Heisenbyte Logo" className="about-logo-image" />
                     </div>
                 </div>
             </div>
